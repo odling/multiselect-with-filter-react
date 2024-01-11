dcfca9874de55c1c9e1f313da5602e187bf978ef
odling <decodeafterme@gmail.com>
2024-01-11 10:31:28 +0300

    [REFACTOR] refactor useAccessibleMultiSelect

--- src/hooks/useAccessibleMultiSelect.ts
+++ src/hooks/useAccessibleMultiSelect.ts
@@ -1,5 +1,112 @@
 import { useEffect, useRef, useState } from "react";
 
+const getTriggerKeydownHandler = (
+  openAction: () => void,
+  closeAction: () => void,
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>
+) => {
+  return (e: KeyboardEvent) => {
+    switch (e.key) {
+      case "Enter":
+      case "Down":
+      case "ArrowDown":
+        /** Open the popover and focus on the first item */
+        e.preventDefault();
+        openAction();
+        setActiveIndex(0);
+        break;
+      case "Up":
+      case "ArrowUp":
+      case "Escape":
+      case "Esc":
+        /** Close the popover and reset active index */
+        e.preventDefault();
+        closeAction();
+        setActiveIndex(-1);
+        break;
+      case "Tab":
+        /** Close the popover and reset active index */
+        /** Do not prevent default */
+        closeAction();
+        setActiveIndex(-1);
+        break;
+      default:
+        setActiveIndex(-1);
+        break;
+    }
+  };
+};
+
+const getItemKeydownHandler = (
+  closeAction: () => void,
+  activeIndex: number,
+  optionsLength: number,
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>,
+  triggerRef: React.RefObject<HTMLInputElement>,
+  itemRef: React.RefObject<HTMLLIElement>
+) => {
+  return (e: KeyboardEvent) => {
+    e.preventDefault();
+    switch (e.key) {
+      case "Up":
+      case "ArrowUp":
+        /** Focus on previous item or the filter element */
+        if (activeIndex === 0) {
+          triggerRef.current?.focus();
+          setActiveIndex(-1);
+        } else {
+          setActiveIndex(activeIndex - 1);
+        }
+        break;
+      case "Down":
+      case "ArrowDown":
+        setActiveIndex((prev) =>
+          /** Focus on next item or the first item */
+          prev + 1 === optionsLength ? 0 : prev + 1
+        );
+        break;
+      case "Esc":
+      case "Escape":
+        /** Close the popover and reset active index */
+        closeAction();
+        setActiveIndex(-1);
+        break;
+      case "PageUp":
+      case "Home":
+        /** Focus on the first item */
+        setActiveIndex(0);
+        break;
+      case "PageDown":
+      case "End":
+        /** Focus on the last item */
+        setActiveIndex(optionsLength - 1);
+        break;
+      case "Enter":
+      case " ": // Space
+        /** Click on the focused item */
+        itemRef.current?.click();
+        break;
+      case "Tab":
+        if (e.shiftKey) {
+          /** ACT AS UP ARROW */
+          /** Focus on previous item or the filter element */
+          if (activeIndex === 0) {
+            triggerRef.current?.focus();
+            setActiveIndex(-1);
+          } else {
+            setActiveIndex(activeIndex - 1);
+          }
+          break;
+        } else {
+          /** ACT AS DOWN ARROW */
+          /** Focus on next item or the first item */
+          setActiveIndex((prev) => (prev + 1 === optionsLength ? 0 : prev + 1));
+          break;
+        }
+    }
+  };
+};
+
 interface IUseAccessibleMultiSelectProps {
   isPopoverVisible: boolean;
   isFocus: boolean;
@@ -21,103 +128,23 @@ const useAccessibleMultiSelect = ({
   useEffect(() => {
     let keyDownCallback: (e: KeyboardEvent) => void;
     if (isFocus) {
-      keyDownCallback = (e) => {
-        switch (e.key) {
-          case "Enter":
-          case "Down":
-          case "ArrowDown":
-            /** Open the popover and focus on the first item */
-            e.preventDefault();
-            openAction();
-            setActiveIndex(0);
-            console.log("Hello");
-            break;
-          case "Up":
-          case "ArrowUp":
-          case "Escape":
-          case "Esc":
-            /** Close the popover and reset active index */
-            e.preventDefault();
-            closeAction();
-            setActiveIndex(-1);
-            break;
-          case "Tab":
-            /** Close the popover and reset active index */
-            /** Do not prevent default */
-            closeAction();
-            setActiveIndex(-1);
-            break;
-          default:
-            setActiveIndex(-1);
-            break;
-        }
-      };
+      keyDownCallback = getTriggerKeydownHandler(
+        openAction,
+        closeAction,
+        setActiveIndex
+      );
       document.addEventListener("keydown", keyDownCallback);
     } else {
       /** attach no listener if trigger or list item is not focused */
       if (activeIndex < 0) return;
-      keyDownCallback = (e) => {
-        e.preventDefault();
-        switch (e.key) {
-          case "Up":
-          case "ArrowUp":
-            /** Focus on previous item or the filter element */
-            if (activeIndex === 0) {
-              triggerRef.current?.focus();
-              setActiveIndex(-1);
-            } else {
-              setActiveIndex(activeIndex - 1);
-            }
-            break;
-          case "Down":
-          case "ArrowDown":
-            setActiveIndex((prev) =>
-              /** Focus on next item or the first item */
-              prev + 1 === optionsLength ? 0 : prev + 1
-            );
-            break;
-          case "Esc":
-          case "Escape":
-            /** Close the popover and reset active index */
-            closeAction();
-            setActiveIndex(-1);
-            break;
-          case "PageUp":
-          case "Home":
-            /** Focus on the first item */
-            setActiveIndex(0);
-            break;
-          case "PageDown":
-          case "End":
-            /** Focus on the last item */
-            setActiveIndex(optionsLength - 1);
-            break;
-          case "Enter":
-          case " ": // Space
-            /** Click on the focused item */
-            itemRef.current?.click();
-            break;
-          case "Tab":
-            if (e.shiftKey) {
-              /** ACT AS UP ARROW */
-              /** Focus on previous item or the filter element */
-              if (activeIndex === 0) {
-                triggerRef.current?.focus();
-                setActiveIndex(-1);
-              } else {
-                setActiveIndex(activeIndex - 1);
-              }
-              break;
-            } else {
-              /** ACT AS DOWN ARROW */
-              /** Focus on next item or the first item */
-              setActiveIndex((prev) =>
-                prev + 1 === optionsLength ? 0 : prev + 1
-              );
-              break;
-            }
-        }
-      };
+      keyDownCallback = getItemKeydownHandler(
+        closeAction,
+        activeIndex,
+        optionsLength,
+        setActiveIndex,
+        triggerRef,
+        itemRef
+      );
       document.addEventListener("keydown", keyDownCallback);
     }
     return () => {
